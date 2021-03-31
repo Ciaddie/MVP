0fd4045719967f442950b3c710ae52129051a8e8
Ciaddie <ciaddiebb@gmail.com>
2021-03-31 10:08:02 -0700

    Attempting to turn unirest to express

--- server/index.js
+++ server/index.js
@@ -1,30 +1,6 @@
-var unirest = require("unirest");
+// var unirest = require("unirest");
 
-var req = unirest("POST", "https://mycookbook-io1.p.rapidapi.com/recipes/rapidapi");
-
-req.headers({
-	"content-type": "text/plain",
-	"x-rapidapi-key": "11a8227713mshdce4817412b6f92p19ef0ejsn937020eeddd5",
-	"x-rapidapi-host": "mycookbook-io1.p.rapidapi.com",
-	"useQueryString": true
-});
-
-req.send("https://www.jamieoliver.com/recipes/vegetables-recipes/superfood-salad/");
-
-
-req.end(function (res) {
-	if (res.error) throw new Error(res.error);
-
-	console.log(res.body);
-});
-
-
-
-
-
-
-
-// const req = unirest("POST", "https://mycookbook-io1.p.rapidapi.com/recipes/rapidapi");
+// var req = unirest("POST", "https://mycookbook-io1.p.rapidapi.com/recipes/rapidapi");
 
 // req.headers({
 // 	"content-type": "text/plain",
@@ -41,3 +17,18 @@ req.end(function (res) {
 
 // 	console.log(res.body);
 // });
+
+const express = require('express');
+const config = require('../config.js');
+
+app = express();
+const port = 3000;
+//const db = require('pathway');
+
+app.use(express.static('public'));
+app.use(express.json());
+
+app.post('https://mycookbook-io1.p.rapidapi.com/recipes/rapidapi', function(req, res) {
+  res.sendStatus(200).send('https://www.jamieoliver.com/recipes/vegetables-recipes/superfood-salad/');
+}
+);
