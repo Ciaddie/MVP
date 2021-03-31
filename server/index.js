c133b94dac4df0ed39af5137a1453f588846ec70
Ciaddie <ciaddiebb@gmail.com>
2021-03-31 10:34:51 -0700

    tech debt: convert unirest to express, hide token

--- server/index.js
+++ server/index.js
@@ -1,34 +1,35 @@
-// var unirest = require("unirest");
-
-// var req = unirest("POST", "https://mycookbook-io1.p.rapidapi.com/recipes/rapidapi");
-
-// req.headers({
-// 	"content-type": "text/plain",
-// 	"x-rapidapi-key": "11a8227713mshdce4817412b6f92p19ef0ejsn937020eeddd5",
-// 	"x-rapidapi-host": "mycookbook-io1.p.rapidapi.com",
-// 	"useQueryString": true
-// });
-
-// req.send("https://www.jamieoliver.com/recipes/vegetables-recipes/superfood-salad/");
-
-
-// req.end(function (res) {
-// 	if (res.error) throw new Error(res.error);
-
-// 	console.log(res.body);
-// });
-
-const express = require('express');
+const unirest = require("unirest");
 const config = require('../config.js');
 
-app = express();
-const port = 3000;
-//const db = require('pathway');
+var req = unirest("POST", "https://mycookbook-io1.p.rapidapi.com/recipes/rapidapi");
 
-app.use(express.static('public'));
-app.use(express.json());
+req.headers({
+	"content-type": "text/plain",
+	"x-rapidapi-key": "11a8227713mshdce4817412b6f92p19ef0ejsn937020eeddd5",
+	"x-rapidapi-host": "mycookbook-io1.p.rapidapi.com",
+	"useQueryString": true
+});
 
-app.post('https://mycookbook-io1.p.rapidapi.com/recipes/rapidapi', function(req, res) {
-  res.sendStatus(200).send('https://www.jamieoliver.com/recipes/vegetables-recipes/superfood-salad/');
-}
-);
+req.send("https://www.jamieoliver.com/recipes/vegetables-recipes/superfood-salad/");
+
+
+req.end(function (res) {
+	if (res.error) throw new Error(res.error);
+
+	console.log(res.body);
+});
+
+// const express = require('express');
+// const config = require('../config.js');
+
+// app = express();
+// const port = 3000;
+// //const db = require('pathway');
+
+// app.use(express.static('public'));
+// app.use(express.json());
+
+// app.post('https://mycookbook-io1.p.rapidapi.com/recipes/rapidapi', function(req, res) {
+//   res.sendStatus(200).send('https://www.jamieoliver.com/recipes/vegetables-recipes/superfood-salad/');
+// }
+// );
